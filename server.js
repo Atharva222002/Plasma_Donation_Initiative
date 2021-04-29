34b24e66a5f205fea0b96c6706276b83a29e0d76
rogaikwad30 <67579967+rogaikwad30@users.noreply.github.com>
2021-04-29 22:23:31 +0530

    Updated PORT

--- server.js
+++ server.js
@@ -31,6 +31,6 @@ app.use('/', require('./routes'));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () =>
+app.listen(process.env.PORT || 5000, () =>
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
